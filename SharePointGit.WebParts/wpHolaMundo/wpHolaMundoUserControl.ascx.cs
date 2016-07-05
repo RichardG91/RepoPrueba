d3623b4bbc6df6aca0c2796b9a96081a1cef8420
Ricardo Vetere <ricardo.vetere@sofrecom.com.ar>
2016-07-05 15:58:22 -0300

    Este es mi primer commit

---
+++ SharePointGit.WebParts/wpHolaMundo/wpHolaMundoUserControl.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+namespace SharePointGit.WebParts.wpHolaMundo
+{
+    public partial class wpHolaMundoUserControl : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
